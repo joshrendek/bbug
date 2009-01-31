2c22aa09c2203a257a2ed6f0ddc99760c9ad17b9
root <josh@bluescripts.net>
2009-01-31 12:18:27 -0500

    XSS check against ha.ckers.org/xss.html

--- view.php
+++ view.php
@@ -41,7 +41,7 @@ if(isset($_POST["submitReport"])){
   
   $bugData = array('id' => 'null', 'project' => $this->db->first("SELECT `project` FROM list WHERE `id`='$bugid'", 0, 0),
   'parent' => $bugid, 'title' => $_POST["subject"], 
-        'report' => nl2br($_POST["report"]), 'status' => '', 'by' => $reportedby, 'priority' => 0, 
+        'report' => nl2br(strip_tags($_POST["report"])), 'status' => '', 'by' => $reportedby, 'priority' => 0, 
         'type' => 0, 'started' => time(), 'finished' => '', 'due' => '', 'assigned' => '', 'attachment' => $target.$file_name);
                 $this->db->query_insert('list', $bugData);
                 $this->message("<center><h3>Reply added.</h3></center>");
